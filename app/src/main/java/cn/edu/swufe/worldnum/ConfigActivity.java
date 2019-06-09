312b309180417f132279cd894b8541d1b296df27
fls123000 <1315481895@qq.com>
2019-06-09 19:52:50 +0800

    第二次修改，写好了国外的部分

--- app/src/main/java/cn/edu/swufe/worldnum/ConfigActivity.java
+++ app/src/main/java/cn/edu/swufe/worldnum/ConfigActivity.java
@@ -1,13 +1,56 @@
 package cn.edu.swufe.worldnum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
-public class ConfigActivity extends AppCompatActivity {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class ConfigActivity extends AppCompatActivity implements Runnable{
+    EditText Num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        Thread t = new Thread(this);
+        t.start();
     }
-}
+
+    @Override
+    public void run() {
+        Document doc;
+        Intent intent = getIntent();
+        String num = intent.getStringExtra("num");
+        try {
+            doc = Jsoup.connect("http://www.51zzl.com/rcsh/guoji.asp").get();
+            Elements tables = doc.getElementsByTag("table");
+            for (int i = 6; i < tables.size(); i ++) {
+                Element table1 = tables.get(i);
+                Elements tds = table1.getElementsByTag("td");
+                for (int n = 4; n < tds.size(); n += 2) {
+                    Element td1 = tds.get(n);
+                    Element td2 = tds.get(n + 1);
+                    String str1 = td1.text();
+                    String str2 = td2.text();
+                    if (num.equals(str2)) {
+                        Num= findViewById(R.id.result);
+                        Num.setText(str1+"==>"+num);
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
--- app/src/main/java/cn/edu/swufe/worldnum/MainActivity.java
+++ app/src/main/java/cn/edu/swufe/worldnum/MainActivity.java
@@ -3,69 +3,80 @@ package cn.edu.swufe.worldnum;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements Runnable{
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {//implements Runnable {
     EditText numtext;
+    private String name = "";
     private String num = "";
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        numtext=findViewById(R.id.num);
+        numtext = findViewById(R.id.num);
         SharedPreferences sharedPreferences = getSharedPreferences("search_num", Activity.MODE_PRIVATE);
         PreferenceManager.getDefaultSharedPreferences(this);
-        num = sharedPreferences.getString ("num", "");
-
+        name = sharedPreferences.getString("name", "");
+        num = sharedPreferences.getString("num", "");
     }
 
     private void openConfig() {
+        String num = numtext.getText().toString();
         Intent config1 = new Intent(this, ConfigActivity.class);
-//        config.putExtra("dollar_rate_key", dollarRate);
-//        config.putExtra("euro_rate_key", euroRate);
-//        config.putExtra("won_rate_key", wonRate);
-        //startActivity(config);
+        config1.putExtra("num", num);
         startActivityForResult(config1, 1);
     }
-    public void onClick(View btn) {
-        String str = numtext.getText().toString();
-        if (str.length() > 0) {
-            if (btn.getId() == R.id.search_chose_btn1) {
-                Intent config1 = new Intent(this, ConfigActivity.class);
-                //config.putExtra("dollar_rate_key", dollarRate)；
-                //startActivity(config);
-                startActivityForResult(config1, 1);
-            } else {
-                Intent config2 = new Intent(this, ConfigActivity2.class);
-//                config.putExtra("dollar_rate_key", dollarRate);
-//                config.putExtra("euro_rate_key", euroRate);
-//                config.putExtra("won_rate_key", wonRate);
-                //startActivity(config);
-                startActivityForResult(config2, 1);
-            }
-            {
-                Intent config = new Intent(this, ConfigActivity.class);
-                //config.putExtra("dollar_rate_key", dollarRate);
-                //config.putExtra("euro_rate_key", euroRate);
-                //config.putExtra("won_rate_key", wonRate);
-                //startActivity(config);
-                startActivityForResult(config, 1);
-            }
 
+    public void onClick(View btn) {
+        String num = numtext.getText().toString();
+        if (num.length() > 0) {
+            if (btn.getId() == R.id.search_chose_btn1) {
+                openConfig();
+            } else {
+                openConfig();
+            }
         } else {
-            Toast.makeText(MainActivity.this, "请输入金额", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "请输入查询的国家或地区区号", Toast.LENGTH_SHORT).show();
         }
 
     }
 
     @Override
-    public void run() {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.num, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.inl_num) {
+            Intent config = new Intent(this, InlNumActivity.class);
+            startActivity(config);
+        } else if (item.getItemId() == R.id.for_num) {
+            Intent list = new Intent(this, ForNumActivity.class);
+            startActivity(list);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
